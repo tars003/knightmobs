2a967f0e312acd1dc295d31a57bc325791171f7d
tars <ajaysharma2ku@gmail.com>
2020-09-11 14:47:22 +0530

    Fixed button redirects

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,17 @@ const mongoose = require('mongoose');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log('index session user:');
-    console.log(req.session.user);
-    res.render('index');
+router.get('/', async(req, res) => {
+    if(req.session.user) {
+        console.log(req.session.user);
+        const user = await User.findById(req.session.user.userId);
+        const payload = {
+            user: user
+        }
+        res.render('index', payload);
+    } else {
+        res.render('index');
+    }
 });
 
 router.post('/', (req, res) => {
